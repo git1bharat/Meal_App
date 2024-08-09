54aca62dd80f010537e825dca1318f3c03c2e91d
BharatYadav <yadavbharat71@gmail.com>
2024-08-10 00:26:11 +0530

    theming and widget implemented on category list screen

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/model/category_model.dart';
+
+const availableCategories = [
+  CategoryModel(id: 'b1', title: 'Irani', color: Colors.pink),
+  CategoryModel(id: 'b2', title: 'Italian', color: Colors.green),
+  CategoryModel(id: 'b3', title: 'Iris', color: Colors.blue),
+  CategoryModel(id: 'b4', title: 'SouthIndian', color: Colors.white),
+  CategoryModel(id: 'b5', title: 'Chines', color: Colors.yellow),
+  CategoryModel(id: 'b6', title: 'Indian', color: Colors.blueGrey),
+  CategoryModel(id: 'b7', title: 'Irani', color: Colors.purple),
+  CategoryModel(id: 'b8', title: 'Italian', color: Colors.orange),
+  CategoryModel(id: 'b9', title: 'Iris', color: Colors.blue),
+  CategoryModel(id: 'b10', title: 'SouthIndian', color: Colors.blueAccent),
+  CategoryModel(id: 'b11', title: 'Chines', color: Colors.red),
+  CategoryModel(
+      id: 'b12', title: 'Indian', color: Color.fromARGB(255, 140, 242, 23)),
+];
---
+++ lib/screens/categories.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick Your Category'),
+      ),
+      body: GridView(
+          padding: EdgeInsets.all(24),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: availableCategories
+              .map((e) => CategoryGridItem(categoryItem: e))
+              .toList()),
+
+      // Alternet Way
+      // for(final e in availableCategories)
+      //CategoryGridItem(categoryItem:e)
+    );
+  }
+}
